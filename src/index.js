d770485650fbc563ab25db3bc29d72644e3c198b
Your Name <hoanganhdo182@gmail.com>
2025-07-01 17:57:34 +0700

    tích hợp Vercel Analytics

--- src/index.js
+++ src/index.js
@@ -2,11 +2,27 @@ import React from 'react';
 import { createRoot } from 'react-dom/client';
 import './index.css';
 import App from './App';
+import { Analytics } from '@vercel/analytics/react';
 // import reportWebVitals from './reportWebVitals';
 
 const domNode = document.getElementById('root');
 const root = createRoot(domNode);
-root.render(<App />);
+root.render(
+  <>
+    <App />
+    <Analytics 
+      mode="production" 
+      debug={false}
+      beforeSend={(event) => {
+        // Loại bỏ các sự kiện từ các địa chỉ IP nội bộ
+        if (window.location.hostname === 'localhost' || window.location.hostname === '127.0.0.1') {
+          return null;
+        }
+        return event;
+      }}
+    />
+  </>
+);
 
 
 // If you want to start measuring performance in your app, pass a function
